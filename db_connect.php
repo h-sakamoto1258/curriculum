18f83dd25fed41c45ae891f3efa5a517a3aef7d4
h-sakamoto1258 <114583749+h-sakamoto1258@users.noreply.github.com>
2023-01-22 23:02:48 +0900

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,9 @@
+<?php
+// DB名
+define('DB_DATABASE', 'checktest4');
+// MySQLのユーザー名
+define('DB_USERNAME', 'root');
+// MySQLのログインパスワード
+define('DB_PASSWORD', 'root');
+// DSN
+define('PDO_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
---
+++ signUp.php
@@ -0,0 +1,47 @@
+<?php
+
+require ('db_connect.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $password = $_POST['password'];  
+}
+
+// $name = trim(filter_input(INPUT_POST, 'name'));
+// $password = trim(filter_input(INPUT_POST, 'password'));
+
+if (isset($name)&&($password)) {
+    echo "登録が完了しました";
+}
+
+function db_connect() {
+    try {
+        $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch(PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+        die();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <h1>新規登録</h1>
+    <form method="POST" action="signUp.php">
+        user:<br>
+        <input type="text" name="name" id="name">
+        <br>
+        password:<br>
+        <input type="password" name="password" id="password"><br>
+        <input type="submit" value="submit" id="signUp" name="signUp">
+    </form>
+</body>
+</html>
