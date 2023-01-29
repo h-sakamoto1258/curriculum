5f7e393472eab296083d811d3821e3604b4bd810
h-sakamoto1258 <114583749+h-sakamoto1258@users.noreply.github.com>
2023-01-30 05:10:41 +0900

    Add files via upload

--- signUp.php
+++ signUp.php
@@ -2,38 +2,52 @@
 
 require_once ('db_connect.php');
 
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-  if (!empty($_POST['name']) && !empty($_POST['password'])) {
-    $named = $_POST['name'];
-    $passed = $_POST['password'];
-// } else {
-//     exit;
-//   }
-}
-// if (isset($named , $passed)) {
-//     echo "登録が完了しました";
-//     }
-$pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+if(isset($_POST["signUp"])) {
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+// if ($_SERVER['REQUEST_METHOD'] === "POST") {
+//   if (!empty($_POST['name']) && !empty($_POST['password'])) {
+//     $named = $_POST['name'];
+//     $passed = $_POST['password'];
 
-// function db_connect() {
-    try {
-        $sql = 'SELECT * FROM users WHERE id < :id';
-        $hash = password_hash($passed, PASSWORD_DEFAULT);
-        $prepare = $pdo->prepare($sql);
-        $pdo->query(
-            "INSERT INTO users (name, password) VALUE($named, $passed)"
-        );
-        $prepare->execute();
-        if (isset($named , $passed)) {
-            echo "登録が完了しました";
-            }
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch(PDOException $e) {
-        echo 'Error: ' . $e->getMessage();
-        die();
+    $sql = "INSERT INTO users (name, password) VALUES ('{$name}','{$password_hash}')";
+
+    $pdo = db_connect();
+
+// $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+
+//     try {
+//         $sql = 'SELECT * FROM users WHERE id < :id';
+//         $hash = password_hash($passed, PASSWORD_DEFAULT);
+//         $prepare = $pdo->prepare($sql);
+//         $pdo->query(
+//             "INSERT INTO users (name, password) VALUE($named, $passed)"
+//         );
+//         $prepare->execute();
+//         if (isset($named , $passed)) {
+//             echo "登録が完了しました";
+//             }
+//         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//         return $pdo;
+//     } catch(PDOException $e) {
+//         echo 'Error: ' . $e->getMessage();
+//         die();
+//     }
+// }
+
+try{
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindParam('name', $name , PDO::PARAM_INT);
+      $stmt->bindParam('password', $password_hash , PDO::PARAM_INT);
+      $stmt->execute();
+      echo "登録が完了しました";
+      
+    }catch(PDOExeption $e){
+      echo 'Error:'.$e->getMessage();
+      die();
     }
-}
+  }
 ?>
 
 
@@ -45,7 +59,7 @@ $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
 </head>
 <body>
     <h1>新規登録</h1>
-    <form method="POST" action="signUp.php">
+    <form method="POST" action="">
         user:<br>
         <input type="text" name="name" id="name">
         <br>
