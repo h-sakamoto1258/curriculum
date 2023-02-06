37378c39b49a5444409e5c2fa821cff25b5ca714
h-sakamoto1258 <114583749+h-sakamoto1258@users.noreply.github.com>
2023-02-06 23:45:01 +0900

    Add files via upload

--- create_post.php
+++ create_post.php
@@ -6,10 +6,12 @@ require_once ('db_connect.php');
 require_once ('function.php');
 
 // ログインしていなければ、login.phpにリダイレクト
-if (!(isset($_SESSION["user_id"]))) {
-    header('Location: login.php'); // ログインしていれば
-    exit; // 処理終了
-}
+check_user_logged_in();
+
+// if (!(isset($_SESSION["user_id"]))) {
+//     header('Location: login.php'); // ログインしていれば
+//     exit; // 処理終了
+// }
 
 // 提出ボタンが押された場合
 // if (isset($_POST['title'] && $_POST['content'])) {
@@ -26,11 +28,11 @@ if (isset($_POST)) {
         $title = htmlspecialchars($_POST['title'], ENT_QUOTES);
         $content = htmlspecialchars($_POST['content'], ENT_QUOTES);
         // PDOのインスタンスを取得
-        $po = db_connect();
+        $pdo = db_connect();
 
         try {
             // SQL文の準備
-            $sql = "SELECT * FROM posts WHERE title = $title && content = $content";
+            $sql = "SELECT * FROM posts WHERE title = :title && content = :content";
             // プリペアドステートメントの準備
             $stmt = $pdo -> prepare($sql);
             // $stmt = $pdo -> prepare("INSERT INTO posts(title , content) VALUE (;title , ;content)");
