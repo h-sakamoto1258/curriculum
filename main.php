5f5f5c21fc53645c25402448470335b0235773e3
h-sakamoto1258 <114583749+h-sakamoto1258@users.noreply.github.com>
2023-03-08 01:05:07 +0900

    Add files via upload

--- main.php
+++ main.php
@@ -95,13 +95,13 @@ if(!empty($_POST["title"]) && !empty($_POST["date"]) && !empty($_POST["stock"]))
         <div>
         <!-- <div class="zaiko"> -->
             <label>
-                <SELECT>
-                    <option>選択してください</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <SELECT name="stock" id="name">
+                    <option value="">--選択してください--</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
                 </SELECT>
             </label>
             <br>
